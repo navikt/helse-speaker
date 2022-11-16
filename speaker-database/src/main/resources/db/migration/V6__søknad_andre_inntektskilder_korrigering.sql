bce2d8f8911aa70b33f56cae9ff7487a65e15846
Helene Arnesen <helene.arnesen@nav.no>
2022-11-16 14:37:49 +0100

    Korrigerer RV_SØ_10

---
+++ speaker-database/src/main/resources/db/migration/V6__søknad_andre_inntektskilder_korrigering.sql
@@ -0,0 +1,6 @@
+INSERT INTO varsel_tittel(varselkode_ref, tittel) VALUES
+    ((SELECT id FROM varselkode WHERE kode = 'RV_SØ_10'), 'Den sykmeldte har fått et nytt inntektsforhold.');
+INSERT INTO varsel_forklaring(varselkode_ref, forklaring) VALUES
+    ((SELECT id FROM varselkode WHERE kode = 'RV_SØ_10'), 'Den sykmeldte har oppgitt i søknaden at han/hun har fått et nytt inntektsforhold i sykmeldingsperioden.');
+INSERT INTO varsel_handling(varselkode_ref, handling) VALUES
+    ((SELECT id FROM varselkode WHERE kode = 'RV_SØ_10'), 'Du må sjekke om sykepengene skal graderes ut ifra inntekt i det nye inntektsforholdet. Du må kontakte bruker for å få informasjon om inntekten og når den er opptjent. Ta stilling til om det bør sendes en oppgave til NAV-kontoret for vurdering av vilkårene i § 8-4.');
