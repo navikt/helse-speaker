a3cdffa2bf1a6bd4880aad3e131af8f4b25ea252
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-03-22 12:41:52 +0100

    💄 Litt designendringer
    Co-authored-by: Solveig Grieg <solveig.grieg@nav.no>

--- speaker-frontend/src/components/NyttVarsel.tsx
+++ speaker-frontend/src/components/NyttVarsel.tsx
@@ -7,6 +7,7 @@ import styles from './NyttVarsel.module.css';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { brukerState, subdomenerOgKonteksterState, varslerState, velgbareTeammeldemmerState } from '../state/state';
 import { Bruker, Subdomene } from '../types';
+import { AddCircle, Close } from '@navikt/ds-icons';
 
 interface NyttVarselForm {
     tittel: string;
@@ -25,6 +26,9 @@ export const NyttVarsel = () => {
 
     const setVarsler = useSetRecoilState(varslerState);
     const bruker = useRecoilValue(brukerState);
+
+    const [toggled, setToggled] = useState(false);
+
     const [isLoading, setIsLoading] = useState(false);
 
     const {
@@ -73,113 +77,134 @@ export const NyttVarsel = () => {
     if (!subdomener) return <></>;
 
     return (
-        <div className={classNames(styles.NyttVarsel, 'p-4 pb-1')}>
-            <div className={'flex flex-col gap-4'}>
-                <Heading level="4" size={'small'}>
-                    Nytt varsel
-                </Heading>
-                <form className={'flex flex-col gap-4'} onSubmit={handleSubmit(onSubmit)}>
-                    <div className={'flex flex-row gap-4 items-start'}>
-                        <Select
-                            className={'min-w-[12rem]'}
-                            label="Subdomene"
-                            error={errors.subdomene ? 'Subdomene påkrevd' : ''}
-                            {...register('subdomene', {
-                                validate: (value) => {
-                                    return !['default', undefined].includes(value);
-                                },
-                            })}
-                        >
-                            <option key={0} value={'default'}>
-                                Velg subdomene
-                            </option>
-                            {subdomener.map((it) => (
-                                <option key={it.forkortelse} value={it.forkortelse}>
-                                    {it.navn} ({it.forkortelse})
-                                </option>
-                            ))}
-                        </Select>
-                        <Select
-                            className={'min-w-[10rem]'}
-                            label="Kontekst"
-                            disabled={erDefaultSubdomene()}
-                            error={errors.kontekst ? 'Kontekst påkrevd' : ''}
-                            {...register('kontekst', {
-                                validate: (value) => {
-                                    return !['default', undefined].includes(value);
-                                },
-                            })}
-                        >
-                            <option key={0} value={'default'}>
-                                Velg kontekst
-                            </option>
-                            {!erDefaultSubdomene() &&
-                                subdomener
-                                    .find((it) => it.forkortelse === selectedSubdomene)
-                                    ?.kontekster.map((kontekst) => {
-                                        return (
-                                            <option key={kontekst.forkortelse} value={kontekst.forkortelse}>
-                                                {kontekst.navn} ({kontekst.forkortelse})
-                                            </option>
-                                        );
-                                    })}
-                        </Select>
-                        <span className={'self-start mt-11'}>Varselkode: {nesteVarselkode ?? ''}</span>
+        <div className={classNames(styles.NyttVarsel, 'pb-5')}>
+            {!toggled ? (
+                <div className="pt-4">
+                    <Button variant="secondary" icon={<AddCircle />} onClick={() => setToggled(true)}>
+                        Legg til nytt varsel
+                    </Button>
+                </div>
+            ) : (
+                <div className={classNames(styles.NyttVarselContainer, 'flex flex-col gap-4 bg-gray-100 pt-1 pr-1')}>
+                    <div className="flex flex-row justify-between items-center">
+                        <Heading level="4" size="small">
+                            Nytt varsel
+                        </Heading>
+                        <Button
+                            variant={'tertiary-neutral'}
+                            title={'Lukk'}
+                            icon={<Close aria-hidden />}
+                            onClick={() => setToggled(false)}
+                        />
                     </div>
-                    <Textarea
-                        label="Tittel"
-                        size="medium"
-                        className={'py-5'}
-                        minRows={1}
-                        maxRows={5}
-                        error={(errors.tittel?.message as string) ?? ''}
-                        {...register('tittel', { required: 'Tittel er påkrevd' })}
-                    />
-                    <Textarea
-                        label="Forklaring"
-                        size="medium"
-                        className={'pb-5'}
-                        minRows={3}
-                        maxRows={5}
-                        {...register('forklaring')}
-                    />
-                    <Textarea
-                        label="Hva gjør man?"
-                        size="medium"
-                        className={'pb-5'}
-                        minRows={3}
-                        maxRows={5}
-                        {...register('handling')}
-                    />
-                    <Label>Medforfattere</Label>
-                    <Chips>
-                        {teammedlemmer.map((c) => (
-                            <Chips.Toggle
-                                selected={selectedMedforfattere.includes(c)}
-                                key={c.oid}
-                                onClick={(event) => {
-                                    event.preventDefault();
-                                    setSelectedMedforfattere(
-                                        selectedMedforfattere.includes(c)
-                                            ? selectedMedforfattere.filter((x) => x !== c)
-                                            : [...selectedMedforfattere, c],
-                                    );
-                                }}
+                    <form className="flex flex-col gap-4 max-w-[1154px]" onSubmit={handleSubmit(onSubmit)}>
+                        <div className="flex flex-row gap-4 items-start">
+                            <Select
+                                className={'min-w-[12rem]'}
+                                label="Subdomene"
+                                error={errors.subdomene ? 'Subdomene påkrevd' : ''}
+                                {...register('subdomene', {
+                                    validate: (value) => {
+                                        return !['default', undefined].includes(value);
+                                    },
+                                })}
                             >
-                                {c.navn}
-                            </Chips.Toggle>
-                        ))}
-                    </Chips>
-                    <div className={'flex flex-row gap-4 pb-5'}>
-                        <Button type={'submit'} variant={'primary'} disabled={!isValid || !isDirty} loading={isLoading}>
-                            Lagre
-                        </Button>
-                        <Button variant={'secondary'} disabled={!isDirty} onClick={() => reset()}>
-                            Avbryt
-                        </Button>
-                    </div>
-                </form>
-            </div>
+                                <option key={0} value={'default'}>
+                                    Velg subdomene
+                                </option>
+                                {subdomener.map((it) => (
+                                    <option key={it.forkortelse} value={it.forkortelse}>
+                                        {it.navn} ({it.forkortelse})
+                                    </option>
+                                ))}
+                            </Select>
+                            <Select
+                                className={'min-w-[10rem]'}
+                                label="Kontekst"
+                                disabled={erDefaultSubdomene()}
+                                error={errors.kontekst ? 'Kontekst påkrevd' : ''}
+                                {...register('kontekst', {
+                                    validate: (value) => {
+                                        return !['default', undefined].includes(value);
+                                    },
+                                })}
+                            >
+                                <option key={0} value={'default'}>
+                                    Velg kontekst
+                                </option>
+                                {!erDefaultSubdomene() &&
+                                    subdomener
+                                        .find((it) => it.forkortelse === selectedSubdomene)
+                                        ?.kontekster.map((kontekst) => {
+                                            return (
+                                                <option key={kontekst.forkortelse} value={kontekst.forkortelse}>
+                                                    {kontekst.navn} ({kontekst.forkortelse})
+                                                </option>
+                                            );
+                                        })}
+                            </Select>
+                            <span className={'self-start mt-11'}>Varselkode: {nesteVarselkode ?? ''}</span>
+                        </div>
+                        <Textarea
+                            label="Tittel"
+                            size="medium"
+                            className={'py-5'}
+                            minRows={1}
+                            maxRows={5}
+                            error={(errors.tittel?.message as string) ?? ''}
+                            {...register('tittel', { required: 'Tittel er påkrevd' })}
+                        />
+                        <Textarea
+                            label="Forklaring"
+                            size="medium"
+                            className={'pb-5'}
+                            minRows={3}
+                            maxRows={5}
+                            {...register('forklaring')}
+                        />
+                        <Textarea
+                            label="Hva gjør man?"
+                            size="medium"
+                            className={'pb-5'}
+                            minRows={3}
+                            maxRows={5}
+                            {...register('handling')}
+                        />
+                        <Label>Medforfattere</Label>
+                        <Chips>
+                            {teammedlemmer.map((c) => (
+                                <Chips.Toggle
+                                    selected={selectedMedforfattere.includes(c)}
+                                    key={c.oid}
+                                    onClick={(event) => {
+                                        event.preventDefault();
+                                        setSelectedMedforfattere(
+                                            selectedMedforfattere.includes(c)
+                                                ? selectedMedforfattere.filter((x) => x !== c)
+                                                : [...selectedMedforfattere, c],
+                                        );
+                                    }}
+                                >
+                                    {c.navn}
+                                </Chips.Toggle>
+                            ))}
+                        </Chips>
+                        <div className={'flex flex-row gap-4 pb-5'}>
+                            <Button
+                                type={'submit'}
+                                variant={'primary'}
+                                disabled={!isValid || !isDirty}
+                                loading={isLoading}
+                            >
+                                Lagre
+                            </Button>
+                            <Button variant={'secondary'} disabled={!isDirty} onClick={() => reset()}>
+                                Avbryt
+                            </Button>
+                        </div>
+                    </form>
+                </div>
+            )}
         </div>
     );
 };
