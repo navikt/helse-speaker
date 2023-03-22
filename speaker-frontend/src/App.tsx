5aca1520b4bb65a420c0aeceddb42e61fbfa500b
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-03-22 08:47:21 +0100

    💄 Endre bruk av tabs

--- speaker-frontend/src/App.tsx
+++ speaker-frontend/src/App.tsx
@@ -2,8 +2,6 @@ import './App.css';
 import '@navikt/ds-css';
 import '@navikt/ds-css-internal';
 import React, { useEffect, useState } from 'react';
-import { Separator } from './components/Separator';
-import { Add, Clock, Dialog, Folder } from '@navikt/ds-icons';
 import { useSetRecoilState } from 'recoil';
 import { Varsler } from './components/Varsler';
 import { Header } from './components/Header';
@@ -35,46 +33,23 @@ const App = () => {
     return (
         <>
             <Header />
-            <div className={'flex flex-row py-4 pr-8 justify-end gap-16'}>
-                <Clock className={'w-[24px] h-[24px]'}></Clock>
-                <Dialog className={'w-[24px] h-[24px]'}></Dialog>
-                <Folder className={'w-[24px] h-[24px]'}></Folder>
-            </div>
-            <Separator />
-            <div className={'max-w-[1275px]'}>
-                <Tabs value={state} onChange={setState}>
-                    <Tabs.List>
-                        <Tabs.Tab value={'varsler'} label={'Varsler'} />
-                        <Tabs.Tab
-                            value="nytt-varsel"
-                            label="Nytt varsel"
-                            icon={<Add aria-hidden title="Nytt varsel" />}
-                        />
-                        <Tabs.Tab
-                            value="nytt-subdomene"
-                            label="Nytt subdomene"
-                            icon={<Add aria-hidden title="Nytt subdomene" />}
-                        />
-                        <Tabs.Tab
-                            value="ny-kontekst"
-                            label="Ny kontekst"
-                            icon={<Add aria-hidden title="Ny kontekst" />}
-                        />
-                    </Tabs.List>
-                    <Tabs.Panel value={'varsler'}>
-                        <Varsler />
-                    </Tabs.Panel>
-                    <Tabs.Panel value="nytt-varsel">
-                        <NyttVarsel />
-                    </Tabs.Panel>
-                    <Tabs.Panel value="nytt-subdomene">
-                        <NyttSubdomene />
-                    </Tabs.Panel>
-                    <Tabs.Panel value="ny-kontekst">
-                        <NyKontekst />
-                    </Tabs.Panel>
-                </Tabs>
-            </div>
+            <Tabs value={state} onChange={setState} className="mt-8">
+                <Tabs.List className="pl-10">
+                    <Tabs.Tab value={'varsler'} label={'Varsler'} />
+                    <Tabs.Tab value="subdomener" label="Subdomener" />
+                    <Tabs.Tab value="kontekster" label="Kontekster" />
+                </Tabs.List>
+                <Tabs.Panel value="varsler" className="pl-10">
+                    <NyttVarsel />
+                    <Varsler />
+                </Tabs.Panel>
+                <Tabs.Panel value="subdomener" className="pl-10">
+                    <NyttSubdomene />
+                </Tabs.Panel>
+                <Tabs.Panel value="kontekster" className="pl-10">
+                    <NyKontekst />
+                </Tabs.Panel>
+            </Tabs>
         </>
     );
 };
