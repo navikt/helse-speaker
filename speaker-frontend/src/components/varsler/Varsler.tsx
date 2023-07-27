c26ce15094e397d2634412b093b64966e9088d9e
Simen Ullern <simenullern@gmail.com>
2023-07-27 09:54:36 +0200

    Kan filtrere ut avviklede varsler i frontend
    Co-authored-by: Eirik Gallefoss <eirik.gallefoss@nav.no>

--- speaker-frontend/src/components/varsler/Varsler.tsx
+++ speaker-frontend/src/components/varsler/Varsler.tsx
@@ -1,15 +1,35 @@
 import { VarselComponent } from './Varsel';
-import React from 'react';
+import React, { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { søkbareVarslerState } from '../../state/state';
 
 export const Varsler = () => {
     const søkbareVarsler = useRecoilValue(søkbareVarslerState);
 
+    const [skalFiltrereAvvikledeVarsler, setSkalFiltrereAvvikledeVarsler] = useState(false)
+
+    const Checkbox = () => {
+        return (
+            <div style={{ color: 'green', lineHeight : 3, padding: 5 }}>
+                <input type="checkbox" id="checkbox" checked={skalFiltrereAvvikledeVarsler} onChange={() => setSkalFiltrereAvvikledeVarsler(!skalFiltrereAvvikledeVarsler)} />
+                <label htmlFor="checkbox">Filtrer bort avviklede varsler</label>
+            </div>
+        )
+    }
+
+    const antallVarsler = skalFiltrereAvvikledeVarsler ? søkbareVarsler.filter(varsel => !varsel.avviklet).length : søkbareVarsler.length
+
     return (
-        <div className={'flex flex-col gap-4'}>
-            {søkbareVarsler.length > 0 &&
-                søkbareVarsler.map((varsel) => <VarselComponent key={varsel.varselkode} varsel={varsel} />)}
+        <div>
+            <Checkbox/>
+            <p style={{color: 'green', lineHeight: 3}}> Viser {antallVarsler} varsler </p>
+            <div className={'flex flex-col gap-4'}>
+                {søkbareVarsler.length > 0 &&
+                    søkbareVarsler.map((varsel) => {
+                        if (varsel.avviklet && skalFiltrereAvvikledeVarsler) return null
+                        return <VarselComponent key={varsel.varselkode} varsel={varsel} />
+                    })}
+            </div>
         </div>
     );
 };
