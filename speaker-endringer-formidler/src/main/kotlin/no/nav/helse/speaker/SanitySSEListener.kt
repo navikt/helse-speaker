5dc6eaeb3ffdda12cce0533258a1b38698b8f94e
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-09-23 15:33:27 +0200

    ⚗️ Shot in the dark på å bruke last-event-id som parameternavn
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- speaker-endringer-formidler/src/main/kotlin/no/nav/helse/speaker/SanitySSEListener.kt
+++ speaker-endringer-formidler/src/main/kotlin/no/nav/helse/speaker/SanitySSEListener.kt
@@ -55,8 +55,8 @@ internal suspend fun sanityVarselendringerListener(
                 parameters.append("includeResult", "true")
                 val lastEventId = bøtte.hentLastEventId()
                 if (lastEventId != null) {
-                    logg.info("Bruker lastEventId i kall: $lastEventId")
-                    parameters.append("lastEventId", lastEventId)
+                    logg.info("Bruker last-event-id i kall: $lastEventId")
+                    parameters.append("last-event-id", lastEventId)
                 }
             }
         },
